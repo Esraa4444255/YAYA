c4a6f9699d30a4a76abc8af3067c6d487d95b967
Esraa4444255 <esraak22@icloud.com>
2025-08-22 18:42:51 +0300

    update YAYA

---
+++ script.js
@@ -0,0 +1,5 @@
+// Change navbar color when scroll
+window.addEventListener("scroll", function() {
+  const nav = document.querySelector(".navbar");
+  nav.classList.toggle("scrolled", window.scrollY > 50);
+});
